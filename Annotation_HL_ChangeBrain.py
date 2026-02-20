e8bf29e8d42e1b6f8644dc1c012f74baea6b9a5d
kana <k.iso113k@gmail.com>
2026-02-20 16:23:03 +0900

    pull前のバックアップ

--- Annotation_HL_ChangeBrain.py
+++ Annotation_HL_ChangeBrain.py
@@ -6,7 +6,7 @@ model = YOLO('yolov8n.pt')
 # 2. 修正したデータでガッツリ学習
 # data.yaml のパスは環境に合わせてください
 model.train(
-    data='E:/program/AI figure learn/yolo/data.yaml', 
+    data='data.yaml', 
     epochs=100,      # 100回学習
     imgsz=640, 
     device='cpu',
@@ -17,5 +17,5 @@ model.train(
     fliplr=0.5,      # 左右反転
     mosaic=1.0,      # 画像合成
     
-    name='clover_retry_v2'  # ★フォルダ名を分かりやすく固定
+    name='komatsuna_first_train'  # ★フォルダ名を分かりやすく固定
 )
--- train_clover.py
+++ train_clover.py
@@ -5,7 +5,7 @@ model = YOLO('yolov8n.pt')
 
 # 学習開始
 model.train(
-    data='E:/program/AI figure learn/yolo/data.yaml', 
+    data='data.yaml',
     epochs=50, 
     imgsz=640, 
     device='cpu'  # NVIDIAのGPUがない場合は 'cpu' でOK
