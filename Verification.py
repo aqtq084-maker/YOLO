7fd33346cfb96e1d19bf8d1c9194f1622e05d7bc
akai <aqyq084@gmail.com>
2026-03-13 14:20:37 +0900

    developからakaiへ

--- Verification.py
+++ Verification.py
@@ -3,13 +3,13 @@ from ultralytics import YOLO
 # 1. 学習完了したモデル(best.pt)を読み込む
 # 先ほどの学習結果が 'train8' にあったので、その中の best.pt を指定します
 # パスの前の 'r' は、Windowsのパス文字化けを防ぐためのおまじないです
-model_path = r'E:\program\AI figure learn\yolo\runs\detect\clover_retry_v32\weights\best.pt'
+model_path = r'E:\program\AI figure learn\yolo\runs\totaldata_weights\best.pt'
 model = YOLO(model_path)
 
 # 2. 推論（予測）したい画像を指定する
 # ここでは例として学習に使った 'clo2.png' を指定しますが、
 # もしスマホなどで撮った「新しいクローバーの画像」があれば、そのパスに書き換えてください！
-image_path = r'E:\program\AI figure learn\yolo\check_result\check_1.jpg'
+image_path = r'E:\program\AI figure learn\yolo\小松菜アプリ\スクリーンショット 2026-03-02 171702.png'
 
 # 3. 推論を実行する
 # save=True : 枠線がついた画像を保存する
