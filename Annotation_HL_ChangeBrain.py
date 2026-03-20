99c9159dee07812c1a03160dd4356b63ef2b9acb
momo_kaihatu <toukasonjuku.ai@gmail.com>
2026-03-20 17:23:55 +0900

    gpu

--- Annotation_HL_ChangeBrain.py
+++ Annotation_HL_ChangeBrain.py
@@ -6,7 +6,7 @@ model = YOLO('yolov8n.pt')
 # 2. 修正したデータでガッツリ学習
 # data.yaml のパスは環境に合わせてください
 model.train(
-    data='E:/program/AI figure learn/yolo/data.yaml', 
+    data='/home/momoshita/協生農法/YOLO/data.yaml', 
     epochs=100,      # 100回学習
     imgsz=640, 
     device='0',
@@ -17,5 +17,5 @@ model.train(
     fliplr=0.5,      # 左右反転
     mosaic=1.0,      # 画像合成
     
-    name='clover_retry_v2'  # ★フォルダ名を分かりやすく固定
+    name='all_1'  # ★フォルダ名を分かりやすく固定
 )
